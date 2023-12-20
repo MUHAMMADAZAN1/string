eed934056a9ff51202d08045d50fde8e83161d34
MUHAMMADAZAN1 <144342317+MUHAMMADAZAN1@users.noreply.github.com>
2023-12-21 00:09:16 +0500

    initialize blankstrings function

--- include/gameFunctions.h
+++ include/gameFunctions.h
@@ -1,32 +1,3 @@
-
-//TODO: define, document, and implement this function
-initializeBlankString();
-
-//TODO: define, document, and implement this function
-printWithSpaces();
-
-//TODO: define, document, and implement this function
-revealGuessedLetter();
-
-//TODO: define, document, and implement this function
-checkGuess();
-
-
-//the following functions have been done for you.
-
-/**
- * Sets the game up, checks for win condition,
- * prints relevant data
- */
-void startGame(char *wordToGuess);
-
-/**
- * Clears the unix terminal of previous input
- */
-void clearScreen();
-
-/**
- * Prints part of the horse based on the ratio between
- * the two numbers.
- */
-void drawHorse(int guessedSoFar, int allowedGuesses);
+// this function take integer for size and a empty string which present garbage value
+// we store string in it
+void initializeBlankString(int length, char *string);
