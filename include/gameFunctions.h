f30a438435947cb84cd6a0abc1f538b26eabb18d
MUHAMMADAZAN1 <144342317+MUHAMMADAZAN1@users.noreply.github.com>
2023-12-21 00:10:35 +0500

    print white spaces

--- include/gameFunctions.h
+++ include/gameFunctions.h
@@ -1,3 +1,6 @@
 // this function take integer for size and a empty string which present garbage value
 // we store string in it
 void initializeBlankString(int length, char *string);
+
+//this function just print space between string character
+void printWithSpaces(const char *str);
